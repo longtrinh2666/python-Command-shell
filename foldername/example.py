9a4176d726a939314702d458d168763ec1d4af9b
longtrinh2666 <longtrinh.2666@gmail.com>
2026-01-10 22:18:40 +0700

    Delete foldername directory

--- foldername/example.py
+++
@@ -1,7 +0,0 @@
-def register(commands):
-    def cmd_hello(args):
-        if args:
-            print("Hello,", " ".join(args))
-        else:
-            print("Hello from plugin 👋")
-    commands["hello"] = cmd_hello
